7a7af7f568a6f86f7286c32de1747550d17ffb4d
HaigEmirzian <hemirzia@stevens.edu>
2024-01-01 23:59:58 -0500

    no idea what this is that I deleted, but if my app works then it works

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-@app.route('/get_portfolio_value')
-# def get_portfolio_value():
-#     updated_portfolio_value = format_dollar(portfolio_value(file_path))  # Replace with your function or logic to get the current value
-#     return jsonify({'portfolio_value': updated_portfolio_value})
